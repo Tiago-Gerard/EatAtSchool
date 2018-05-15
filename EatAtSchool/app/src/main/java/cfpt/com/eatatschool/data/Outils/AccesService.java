170f032fda33792d4072824e9c667b1f90de9037
Unknown <ottavio.bnm@eduge.ch>
2018-05-15 07:57:47 +0200

    Ajout du header dans la requête HTTP
    Le header ne fonctionne pas encore correctement

--- EatAtSchool/app/src/main/java/cfpt/com/eatatschool/data/Outils/AccesService.java
+++ EatAtSchool/app/src/main/java/cfpt/com/eatatschool/data/Outils/AccesService.java
@@ -1,9 +1,15 @@
 package cfpt.com.eatatschool.data.Outils;
 
+import java.io.IOException;
 import java.util.List;
 
 import cfpt.com.eatatschool.domaine.RestaurantSerializable;
 import cfpt.com.eatatschool.domaine.SchoolSerializable;
+import okhttp3.Credentials;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -14,6 +20,22 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface AccesService {
+
+
+    //TODO : AUTH à régler
+    OkHttpClient okHttpClient = new OkHttpClient().newBuilder().addInterceptor(new Interceptor() {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request originalRequest = chain.request();
+
+            Request.Builder builder = originalRequest.newBuilder().header("Authorization",
+                    Credentials.basic("aUsername", "aPassword"));
+
+            Request newRequest = builder.build();
+            return chain.proceed(newRequest);
+        }
+    }).build();
+
     @GET("php/ecoles/")
     Call<List<SchoolSerializable>> getEcoles();
     Retrofit retrofitEcoles = new Retrofit.Builder()
@@ -47,6 +69,7 @@ public interface AccesService {
             @Field("lon") double lon);
     Retrofit retrofitAddRestaurant = new Retrofit.Builder()
             .baseUrl("http://10.134.98.158/")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 }
